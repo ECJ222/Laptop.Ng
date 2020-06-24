ec8edec4a4eb8c6f1602a1f8ba946963c88a2a17
enoch chejieh <enochchejieh@gmail.com>
2020-06-24 16:05:05 +0100

    adding change

---
+++ client/build/precache-manifest.9e28cd232fc2f5fda72bba35cc25abce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525fd8983ac32e8f1d5879104da173b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a536c1dd5eb2119f6120",
+    "url": "/static/css/main.3ed54b48.chunk.css"
+  },
+  {
+    "revision": "9a9a0fb0812246c8ff47",
+    "url": "/static/js/2.d77c428b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d77c428b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a536c1dd5eb2119f6120",
+    "url": "/static/js/main.8e9f1bf5.chunk.js"
+  },
+  {
+    "revision": "46c98954bd2824ddd91d",
+    "url": "/static/js/runtime-main.a62328e8.js"
+  }
+]);
